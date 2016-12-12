05e5680eeb22faf2012cd5bc14c1c0c316785d4b
kimhohyun21 <kimhohyun21@gmail.com>
2016-12-13 04:28:01 +0900

    update lesson8~10

--- src/Lesson6/Lesson6_09.java
+++ src/Lesson6/Lesson6_09.java
@@ -1,5 +1,21 @@
 package Lesson6;
 
-public class Lesson6_09 {
+class Data{ int x; }
 
+public class Lesson6_09 {
+	public static void main(String[] args){
+		Data data=new Data();
+		data.x=10; 
+		System.out.println("main() : x = "+data.x);
+		
+		change(data.x); 
+		// change()에서 기본형 매개변수로 값을 읽어들이기만 하고 새로운 값으로 반영하지 못함
+		System.out.println("After change(data.x)");
+		System.out.println("main() : x = "+data.x); 
+	}
+	
+	static void change(int x){ // int x ==> 기본형 매개변수
+		x=1000;
+		System.out.println("change() : x = "+x);
+	}
 }
